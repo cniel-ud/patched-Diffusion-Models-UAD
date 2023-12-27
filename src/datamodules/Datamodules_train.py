4238d206184dc4ff5ac16b33e66bf44fd5069b4d
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2023-12-27 10:34:53 -0500

    make brats training dataset

--- src/datamodules/Datamodules_train.py
+++ src/datamodules/Datamodules_train.py
@@ -7,65 +7,105 @@ import pandas as pd
 
 class IXI(LightningDataModule):
 
-    def __init__(self, cfg, fold = None):
+    def __init__(self, cfg, fold=None):
         super(IXI, self).__init__()
         self.cfg = cfg
-        self.preload = cfg.get('preload',True)
+        self.preload = cfg.get('preload', True)
         # load data paths and indices
         # IXI
 
-        self.cfg.permute = False # no permutation for IXI
-
+        self.cfg.permute = False  # no permutation for IXI
 
         self.imgpath = {}
         self.csvpath_train = cfg.path.IXI.IDs.train[fold]
         self.csvpath_val = cfg.path.IXI.IDs.val[fold]
         self.csvpath_test = cfg.path.IXI.IDs.test
         self.csv = {}
-        states = ['train','val','test']
+        states = ['train', 'val', 'test']
 
         self.csv['train'] = pd.read_csv(self.csvpath_train)
         self.csv['val'] = pd.read_csv(self.csvpath_val)
         self.csv['test'] = pd.read_csv(self.csvpath_test)
         if cfg.mode == 't2':
-            keep_t2 = pd.read_csv(cfg.path.IXI.keep_t2) # only keep t2 images that have a t1 counterpart
+            keep_t2 = pd.read_csv(cfg.path.IXI.keep_t2)  # only keep t2 images that have a t1 counterpart
 
         for state in states:
             self.csv[state]['settype'] = state
             self.csv[state]['setname'] = 'IXI'
 
-
             self.csv[state]['img_path'] = cfg.path.pathBase + '/Data/' + self.csv[state]['img_path']
             self.csv[state]['mask_path'] = cfg.path.pathBase + '/Data/' + self.csv[state]['mask_path']
             self.csv[state]['seg_path'] = None
 
-            if cfg.mode == 't2': 
-                self.csv[state] = self.csv[state][self.csv[state].img_name.isin(keep_t2['0'].str.replace('t2','t1'))]
-                self.csv[state]['img_path'] = self.csv[state]['img_path'].str.replace('t1','t2')
+            if cfg.mode == 't2':
+                self.csv[state] = self.csv[state][self.csv[state].img_name.isin(keep_t2['0'].str.replace('t2', 't1'))]
+                self.csv[state]['img_path'] = self.csv[state]['img_path'].str.replace('t1', 't2')
 
     def setup(self, stage: Optional[str] = None):
         # called on every GPU
-        if not hasattr(self,'train'):
-            if self.cfg.sample_set: # for debugging
-                self.train = create_dataset.Train(self.csv['train'][0:50],self.cfg) 
-                self.val = create_dataset.Train(self.csv['val'][0:50],self.cfg)
-                self.val_eval = create_dataset.Eval(self.csv['val'][0:8],self.cfg)
-                self.test_eval = create_dataset.Eval(self.csv['test'][0:8],self.cfg)
-            else: 
-                self.train = create_dataset.Train(self.csv['train'],self.cfg) 
-                self.val = create_dataset.Train(self.csv['val'],self.cfg)
-                self.val_eval = create_dataset.Eval(self.csv['val'],self.cfg)
-                self.test_eval = create_dataset.Eval(self.csv['test'],self.cfg)
-    
+        if not hasattr(self, 'train'):
+            if self.cfg.sample_set:  # for debugging
+                self.train = create_dataset.Train(self.csv['train'][0:50], self.cfg)
+                self.val = create_dataset.Train(self.csv['val'][0:50], self.cfg)
+                self.val_eval = create_dataset.Eval(self.csv['val'][0:8], self.cfg)
+                self.test_eval = create_dataset.Eval(self.csv['test'][0:8], self.cfg)
+            else:
+                self.train = create_dataset.Train(self.csv['train'], self.cfg)
+                self.val = create_dataset.Train(self.csv['val'], self.cfg)
+                self.val_eval = create_dataset.Eval(self.csv['val'], self.cfg)
+                self.test_eval = create_dataset.Eval(self.csv['test'], self.cfg)
+
     def train_dataloader(self):
-        return DataLoader(self.train, batch_size=self.cfg.batch_size, num_workers=self.cfg.num_workers, pin_memory=True, shuffle=True, drop_last=self.cfg.get('droplast',False))
+        return DataLoader(self.train, batch_size=self.cfg.batch_size, num_workers=self.cfg.num_workers, pin_memory=True,
+                          shuffle=True, drop_last=self.cfg.get('droplast', False))
 
     def val_dataloader(self):
-        return DataLoader(self.val, batch_size=self.cfg.batch_size, num_workers=self.cfg.num_workers, pin_memory=True, shuffle=False)
+        return DataLoader(self.val, batch_size=self.cfg.batch_size, num_workers=self.cfg.num_workers, pin_memory=True,
+                          shuffle=False)
 
     def val_eval_dataloader(self):
         return DataLoader(self.val_eval, batch_size=1, num_workers=self.cfg.num_workers, pin_memory=True, shuffle=False)
 
     def test_eval_dataloader(self):
-        return DataLoader(self.test_eval, batch_size=1, num_workers=self.cfg.num_workers, pin_memory=True, shuffle=False)
-      
+        return DataLoader(self.test_eval, batch_size=1, num_workers=self.cfg.num_workers, pin_memory=True,
+                          shuffle=False)
+
+
+class Brats(LightningDataModule):
+    def __init__(self, cfg, fold=None):
+        super(IXI, self).__init__()
+        self.cfg = cfg
+        self.preload = cfg.get('preload', True)
+
+        self.cfg.permute = False  # no permutation for IXI
+        self.brats_images_train = cfg.brats_train_path
+        self.brats_images_val = cfg.brats_val_path
+        self.brats_images_test_val = cfg.brats_images_test_val
+        self.brats_images_test_test = cfg.brats_images_test_test
+        states = ['train', 'val', 'test']
+
+    def setup(self, stage: Optional[str] = None):
+        # called on every GPU
+        if not hasattr(self, 'train'):
+            if self.cfg.sample_set:  # for debugging
+                raise NotImplementedError("this is not implemented yet.")
+            else:
+                self.train = create_dataset.TrainBrats(self.brats_images_train)
+                self.val = create_dataset.TrainBrats(self.brats_images_val)
+                self.val_eval = create_dataset.EvalBrats(self.brats_images_test_val)
+                self.test_eval = create_dataset.EvalBrats(self.brats_images_test_test)
+
+    def train_dataloader(self):
+        return DataLoader(self.train, batch_size=self.cfg.batch_size, num_workers=self.cfg.num_workers, pin_memory=True,
+                          shuffle=True, drop_last=self.cfg.get('droplast', False))
+
+    def val_dataloader(self):
+        return DataLoader(self.val, batch_size=self.cfg.batch_size, num_workers=self.cfg.num_workers, pin_memory=True,
+                          shuffle=False)
+
+    def val_eval_dataloader(self):
+        return DataLoader(self.val_eval, batch_size=1, num_workers=self.cfg.num_workers, pin_memory=True, shuffle=False)
+
+    def test_eval_dataloader(self):
+        return DataLoader(self.test_eval, batch_size=1, num_workers=self.cfg.num_workers, pin_memory=True,
+                          shuffle=False)
--- src/datamodules/create_dataset.py
+++ src/datamodules/create_dataset.py
@@ -1,78 +1,86 @@
-
 from torch.utils.data import Dataset
-import numpy as np
 import torch
 import SimpleITK as sitk
 import torchio as tio
+import os
+
 sitk.ProcessObject.SetGlobalDefaultThreader("Platform")
 from multiprocessing import Manager
 
-def Train(csv,cfg,preload=True):
+
+def Train(csv, cfg, preload=True):
     subjects = []
     for _, sub in csv.iterrows():
         subject_dict = {
-            'vol' : tio.ScalarImage(sub.img_path,reader=sitk_reader), 
-            'age' : sub.age,
-            'ID' : sub.img_name,
-            'label' : sub.label,
-            'Dataset' : sub.setname,
-            'stage' : sub.settype,
-            'path' : sub.img_path
+            'vol': tio.ScalarImage(sub.img_path, reader=sitk_reader),
+            'age': sub.age,
+            'ID': sub.img_name,
+            'label': sub.label,
+            'Dataset': sub.setname,
+            'stage': sub.settype,
+            'path': sub.img_path
         }
-        if sub.mask_path != None: # if we have masks
-            subject_dict['mask'] = tio.LabelMap(sub.mask_path,reader=sitk_reader)
-        else: # if we don't have masks, we create a mask from the image
-            subject_dict['mask'] = tio.LabelMap(tensor=tio.ScalarImage(sub.img_path,reader=sitk_reader).data>0)
+        if sub.mask_path != None:  # if we have masks
+            subject_dict['mask'] = tio.LabelMap(sub.mask_path, reader=sitk_reader)
+        else:  # if we don't have masks, we create a mask from the image
+            subject_dict['mask'] = tio.LabelMap(tensor=tio.ScalarImage(sub.img_path, reader=sitk_reader).data > 0)
 
         subject = tio.Subject(subject_dict)
         subjects.append(subject)
-    
-    if preload: 
+
+    if preload:
         manager = Manager()
         cache = DatasetCache(manager)
-        ds = tio.SubjectsDataset(subjects, transform = get_transform(cfg))
-        ds = preload_wrapper(ds, cache, augment = get_augment(cfg))
-    else: 
-        ds = tio.SubjectsDataset(subjects, transform = tio.Compose([get_transform(cfg),get_augment(cfg)]))
-        
+        ds = tio.SubjectsDataset(subjects, transform=get_transform(cfg))
+        ds = preload_wrapper(ds, cache, augment=get_augment(cfg))
+    else:
+        ds = tio.SubjectsDataset(subjects, transform=tio.Compose([get_transform(cfg), get_augment(cfg)]))
     if cfg.spatialDims == '2D':
-        slice_ind = cfg.get('startslice',None) 
-        seq_slices = cfg.get('sequentialslices',None) 
-        ds = vol2slice(ds,cfg,slice=slice_ind,seq_slices=seq_slices)
+        slice_ind = cfg.get('startslice', None)
+        seq_slices = cfg.get('sequentialslices', None)
+        ds = vol2slice(ds, cfg, slice=slice_ind, seq_slices=seq_slices)
     return ds
- 
-def Eval(csv,cfg): 
+
+
+def Eval(csv, cfg):
     subjects = []
     for _, sub in csv.iterrows():
-        if sub.mask_path is not None and tio.ScalarImage(sub.img_path,reader=sitk_reader).shape != tio.ScalarImage(sub.mask_path,reader=sitk_reader).shape:
-            print(f'different shapes of vol and mask detected. Shape vol: {tio.ScalarImage(sub.img_path,reader=sitk_reader).shape}, shape mask: {tio.ScalarImage(sub.mask_path,reader=sitk_reader).shape} \nsamples will be resampled to the same dimension')
-            
+        if sub.mask_path is not None and tio.ScalarImage(sub.img_path, reader=sitk_reader).shape != tio.ScalarImage(
+                sub.mask_path, reader=sitk_reader).shape:
+            print(
+                f'different shapes of vol and mask detected. Shape vol: {tio.ScalarImage(sub.img_path, reader=sitk_reader).shape}, shape mask: {tio.ScalarImage(sub.mask_path, reader=sitk_reader).shape} \nsamples will be resampled to the same dimension')
+
         subject_dict = {
-            'vol' : tio.ScalarImage(sub.img_path,reader=sitk_reader),
-            'vol_orig' : tio.ScalarImage(sub.img_path,reader=sitk_reader), # we need the image in original size for evaluation
-            'age' : sub.age,
-            'ID' : sub.img_name,
-            'label' : sub.label,
-            'Dataset' : sub.setname,
-            'stage' : sub.settype,
+            'vol': tio.ScalarImage(sub.img_path, reader=sitk_reader),
+            'vol_orig': tio.ScalarImage(sub.img_path, reader=sitk_reader),
+            # we need the image in original size for evaluation
+            'age': sub.age,
+            'ID': sub.img_name,
+            'label': sub.label,
+            'Dataset': sub.setname,
+            'stage': sub.settype,
             'seg_available': False,
-            'path' : sub.img_path }
-        if sub.seg_path != None: # if we have segmentations
-            subject_dict['seg'] = tio.LabelMap(sub.seg_path,reader=sitk_reader),
-            subject_dict['seg_orig'] = tio.LabelMap(sub.seg_path,reader=sitk_reader)# we need the image in original size for evaluation
+            'path': sub.img_path}
+        if sub.seg_path != None:  # if we have segmentations
+            subject_dict['seg'] = tio.LabelMap(sub.seg_path, reader=sitk_reader),
+            subject_dict['seg_orig'] = tio.LabelMap(sub.seg_path,
+                                                    reader=sitk_reader)  # we need the image in original size for evaluation
             subject_dict['seg_available'] = True
-        if sub.mask_path != None: # if we have masks
-            subject_dict['mask'] = tio.LabelMap(sub.mask_path,reader=sitk_reader)
-            subject_dict['mask_orig'] = tio.LabelMap(sub.mask_path,reader=sitk_reader)# we need the image in original size for evaluation
-        else: 
-            tens=tio.ScalarImage(sub.img_path,reader=sitk_reader).data>0
+        if sub.mask_path != None:  # if we have masks
+            subject_dict['mask'] = tio.LabelMap(sub.mask_path, reader=sitk_reader)
+            subject_dict['mask_orig'] = tio.LabelMap(sub.mask_path,
+                                                     reader=sitk_reader)  # we need the image in original size for evaluation
+        else:
+            tens = tio.ScalarImage(sub.img_path, reader=sitk_reader).data > 0
             subject_dict['mask'] = tio.LabelMap(tensor=tens)
             subject_dict['mask_orig'] = tio.LabelMap(tensor=tens)
 
         subject = tio.Subject(subject_dict)
         subjects.append(subject)
-    ds = tio.SubjectsDataset(subjects, transform = get_transform(cfg))
+    ds = tio.SubjectsDataset(subjects, transform=get_transform(cfg))
     return ds
+
+
 ## got it from https://discuss.pytorch.org/t/best-practice-to-cache-the-entire-dataset-during-first-epoch/19608/12
 class DatasetCache(object):
     def __init__(self, manager, use_cache=True):
@@ -102,18 +110,21 @@ class DatasetCache(object):
             return
         self._dict[str(key)] = (subject)
 
+
 class preload_wrapper(Dataset):
-    def __init__(self,ds,cache,augment=None):
-            self.cache = cache
-            self.ds = ds
-            self.augment = augment
+    def __init__(self, ds, cache, augment=None):
+        self.cache = cache
+        self.ds = ds
+        self.augment = augment
+
     def reset_memory(self):
         self.cache.reset()
+
     def __len__(self):
-            return len(self.ds)
-            
+        return len(self.ds)
+
     def __getitem__(self, index):
-        if self.cache.is_cached(index) :
+        if self.cache.is_cached(index):
             subject = self.cache.get(index)
         else:
             subject = self.ds.__getitem__(index)
@@ -122,31 +133,33 @@ class preload_wrapper(Dataset):
             subject = self.augment(subject)
         return subject
 
+
 class vol2slice(Dataset):
-    def __init__(self,ds,cfg,onlyBrain=False,slice=None,seq_slices=None):
-            self.ds = ds
-            self.onlyBrain = onlyBrain
-            self.slice = slice
-            self.seq_slices = seq_slices
-            self.counter = 0 
-            self.ind = None
-            self.cfg = cfg
+    def __init__(self, ds, cfg, onlyBrain=False, slice=None, seq_slices=None):
+        self.ds = ds
+        self.onlyBrain = onlyBrain
+        self.slice = slice
+        self.seq_slices = seq_slices
+        self.counter = 0
+        self.ind = None
+        self.cfg = cfg
 
     def __len__(self):
-            return len(self.ds)
-            
+        return len(self.ds)
+
     def __getitem__(self, index):
         subject = self.ds.__getitem__(index)
         if self.onlyBrain:
             start_ind = None
             for i in range(subject['vol'].data.shape[-1]):
-                if subject['mask'].data[0,:,:,i].any() and start_ind is None: # only do this once
-                    start_ind = i 
-                if not subject['mask'].data[0,:,:,i].any() and start_ind is not None: # only do this when start_ind is set
-                    stop_ind = i 
+                if subject['mask'].data[0, :, :, i].any() and start_ind is None:  # only do this once
+                    start_ind = i
+                if not subject['mask'].data[0, :, :,
+                       i].any() and start_ind is not None:  # only do this when start_ind is set
+                    stop_ind = i
             low = start_ind
             high = stop_ind
-        else: 
+        else:
             low = 0
             high = subject['vol'].data.shape[-1]
         if self.slice is not None:
@@ -154,58 +167,121 @@ class vol2slice(Dataset):
             if self.seq_slices is not None:
                 low = self.ind
                 high = self.ind + self.seq_slices
-                self.ind = torch.randint(low,high,size=[1])
+                self.ind = torch.randint(low, high, size=[1])
         else:
-            if self.cfg.get('unique_slice',False): # if all slices in one batch need to be at the same location
-                if self.counter % self.cfg.batch_size == 0 or self.ind is None: # only change the index when changing to new batch
-                    self.ind = torch.randint(low,high,size=[1])
-                self.counter = self.counter +1
-            else: 
-                self.ind = torch.randint(low,high,size=[1])
+            if self.cfg.get('unique_slice', False):  # if all slices in one batch need to be at the same location
+                if self.counter % self.cfg.batch_size == 0 or self.ind is None:  # only change the index when changing to new batch
+                    self.ind = torch.randint(low, high, size=[1])
+                self.counter = self.counter + 1
+            else:
+                self.ind = torch.randint(low, high, size=[1])
 
         subject['ind'] = self.ind
 
-        subject['vol'].data = subject['vol'].data[...,self.ind]
-        subject['mask'].data = subject['mask'].data[...,self.ind]
+        subject['vol'].data = subject['vol'].data[..., self.ind]
+        subject['mask'].data = subject['mask'].data[..., self.ind]
 
         return subject
 
 
-def get_transform(cfg): # only transforms that are applied once before preloading
-    h, w, d = tuple(cfg.get('imageDim',(160,192,160)))
+def get_transform(cfg):  # only transforms that are applied once before preloading
+    h, w, d = tuple(cfg.get('imageDim', (160, 192, 160)))
 
-    if not cfg.resizedEvaluation: 
-        exclude_from_resampling = ['vol_orig','mask_orig','seg_orig']
-    else: 
+    if not cfg.resizedEvaluation:
+        exclude_from_resampling = ['vol_orig', 'mask_orig', 'seg_orig']
+    else:
         exclude_from_resampling = None
-        
-    if cfg.get('unisotropic_sampling',True):
+
+    if cfg.get('unisotropic_sampling', True):
         preprocess = tio.Compose([
-        tio.CropOrPad((h,w,d),padding_mode=0),
-        tio.RescaleIntensity((0, 1),percentiles=(cfg.get('perc_low',1),cfg.get('perc_high',99)),masking_method='mask'),
-        tio.Resample(cfg.get('rescaleFactor',3.0),image_interpolation='bspline',exclude=exclude_from_resampling),#,exclude=['vol_orig','mask_orig','seg_orig']), # we do not want to resize *_orig volumes
+            tio.CropOrPad((h, w, d), padding_mode=0),
+            tio.RescaleIntensity((0, 1), percentiles=(cfg.get('perc_low', 1), cfg.get('perc_high', 99)),
+                                 masking_method='mask'),
+            tio.Resample(cfg.get('rescaleFactor', 3.0), image_interpolation='bspline', exclude=exclude_from_resampling),
+            # ,exclude=['vol_orig','mask_orig','seg_orig']), # we do not want to resize *_orig volumes
         ])
 
-    else: 
+    else:
         preprocess = tio.Compose([
-                tio.RescaleIntensity((0, 1),percentiles=(cfg.get('perc_low',1),cfg.get('perc_high',99)),masking_method='mask'),
-                tio.Resample(cfg.get('rescaleFactor',3.0),image_interpolation='bspline',exclude=exclude_from_resampling),#,exclude=['vol_orig','mask_orig','seg_orig']), # we do not want to resize *_orig volumes
-            ])
+            tio.RescaleIntensity((0, 1), percentiles=(cfg.get('perc_low', 1), cfg.get('perc_high', 99)),
+                                 masking_method='mask'),
+            tio.Resample(cfg.get('rescaleFactor', 3.0), image_interpolation='bspline', exclude=exclude_from_resampling),
+            # ,exclude=['vol_orig','mask_orig','seg_orig']), # we do not want to resize *_orig volumes
+        ])
+
+    return preprocess
 
 
-    return preprocess 
-
-def get_augment(cfg): # augmentations that may change every epoch
+def get_augment(cfg):  # augmentations that may change every epoch
     augmentations = []
 
     # individual augmentations
     augment = tio.Compose(augmentations)
     return augment
 
+
 def sitk_reader(path):
-                
     image_nii = sitk.ReadImage(str(path), sitk.sitkFloat32)
-    if not 'mask' in str(path) and not 'seg' in str(path) : # only for volumes / scalar images
-        image_nii = sitk.CurvatureFlow(image1 = image_nii, timeStep = 0.125, numberOfIterations = 3)
-    vol = sitk.GetArrayFromImage(image_nii).transpose(2,1,0)
+    if not 'mask' in str(path) and not 'seg' in str(path):  # only for volumes / scalar images
+        image_nii = sitk.CurvatureFlow(image1=image_nii, timeStep=0.125, numberOfIterations=3)
+    vol = sitk.GetArrayFromImage(image_nii).transpose(2, 1, 0)
     return vol, None
+
+
+def exclude_abnomral_slices(image, mask, slice_dim=0):
+    no_abnormal_image = []
+    if slice_dim == -1:
+        for i in range(image.shape[:-1]):
+            if mask[..., i].mean() < .1:
+                no_abnormal_image.append(image[..., i])
+    else:
+        raise NotImplementedError(f'slice_dim = {slice_dim} is not supported')
+    return torch.stack(no_abnormal_image)
+
+
+def TrainBrats(self, images_path: str, cfg):
+    # Assuming images and masks have the same naming convention and are in the same order
+
+    # Get a list of image files
+    image_files = sorted([f for f in os.listdir(images_path) if f.endswith('.nii')])
+
+    # Get a list of corresponding mask files
+    mask_files = sorted([f.replace('t1', 'seg') for f in image_files])
+    subjects = []
+    for img_file, mask_file in zip(image_files, mask_files):
+        # Read MRI images using tio
+        image = tio.ScalarImage(os.path.join(images_path, img_file))
+        mask = tio.LabelMap(os.path.join(images_path, mask_file))
+
+        # Call the preprocessing method
+        image = exclude_abnomral_slices(image, mask)
+        subject_dict = {'vol': tio.ScalarImage(tensor=image), 'age': None, 'ID': img_file, 'label': None,
+                        'Dataset': None, 'stage': 'train', 'path': img_file, 'mask': tio.LabelMap(tensor=image > .001)}
+        subject = tio.Subject(subject_dict)
+        subjects.append(subject)
+
+    ds = tio.SubjectsDataset(subjects, transform=tio.Compose([get_transform(cfg), get_augment(cfg)]))
+
+
+def EvalBrats(images_path: str, cfg):
+    subjects = []
+    # Get a list of image files
+    image_files = sorted([f for f in os.listdir(images_path) if f.endswith('.nii')])
+
+    # Get a list of corresponding mask files
+    mask_files = sorted([f.replace('t1', 'seg') for f in image_files])
+    subjects = []
+    for img_file, mask_file in zip(image_files, mask_files):
+        # Read MRI images using tio
+        image = tio.ScalarImage(os.path.join(images_path, img_file))
+        mask = tio.LabelMap(os.path.join(images_path, mask_file))
+
+        subject_dict = {'vol': tio.ScalarImage(tensor=image), 'vol_orig': tio.ScalarImage(tensor=image),
+                        'age': None, 'ID': img_file, 'label': None,
+                        'Dataset': None, 'stage': 'train', 'path': img_file, 'mask': tio.LabelMap(tensor=image > .001),
+                        'mask_orig': tio.LabelMap(tensor=image > .001),
+                        'seg_available': True, 'seg': tio.LabelMap(tensor=mask), 'seg_orig': tio.LabelMap(tensor=mask)}
+        subject = tio.Subject(subject_dict)
+        subjects.append(subject)
+    ds = tio.SubjectsDataset(subjects, transform=get_transform(cfg))
+    return ds
