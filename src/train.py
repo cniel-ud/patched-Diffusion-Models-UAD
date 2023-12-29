0db4061fc415f970010f049e7e1daacae0e1fce5
bakerhassan <61099003+bakerhassan@users.noreply.github.com>
2023-12-29 11:45:21 -0500

    Update train.py

--- src/train.py
+++ src/train.py
@@ -143,8 +143,8 @@ def train(cfg: DictConfig) -> Optional[float]:
         # logging
         log.info(f"Best checkpoint path:\n{trainer.checkpoint_callback.best_model_path}")
         log.info(f"Best checkpoint metric:\n{trainer.checkpoint_callback.best_model_score}")
-        trainer.logger.experiment[0].log({'best_ckpt_path': trainer.checkpoint_callback.best_model_path})
-        trainer.logger.experiment[0].log({'logdir': trainer.log_dir})
+        trainer.logger.experiment.log({'best_ckpt_path': trainer.checkpoint_callback.best_model_path})
+        trainer.logger.experiment.log({'logdir': trainer.log_dir})
 
         # metrics
         validation_metrics = trainer.callback_metrics
@@ -202,7 +202,7 @@ def train(cfg: DictConfig) -> Optional[float]:
                 log_dict.update(utils.summarize(preds_dict['test'][set],
                                                 'test'))  # sets prefix test/ and removes lists for better logging in wandb
                 log_dict = utils.summarize(log_dict, f'{fold + 1}/' + set)  # sets prefix for each data set
-                trainer.logger.experiment[0].log(log_dict)
+                trainer.logger.experiment.log(log_dict)
 
             # pickle preds_dict for later analysis
             if cfg.get('pickle_preds', True):
